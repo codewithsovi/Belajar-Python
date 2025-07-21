ed3d37a9ca306c3aa6b7dc456890c768786a1c89
sovi <sovialfina@gmail.com>
2025-07-21 19:12:15 +0700

    percabangan if else

---
+++ latihan1/6ekpresi/ekspresi.py
@@ -0,0 +1,3 @@
+# ekspresi ada 2 macam yaitu biner dan uner
+
+# 1. biner >> memili 2 operan sedangkan uner memiliki 1 operan saja
---
+++ latihan1/7Percabangan/if-else.py
@@ -0,0 +1,13 @@
+ketersediaan = "daging ayam"
+
+# if-else
+if ketersediaan == "daging ayam":
+    print("iya benar, daging ayam")
+else:
+    print("tidak ada daging ayam")
+
+# if-else-if
+if ketersediaan == 'daging kambing':
+    print("daging kambing")
+elif ketersediaan == 'daing ayam':
+    print("daging ayam")

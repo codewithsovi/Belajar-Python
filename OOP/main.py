0c2e01a0079a9fa5d201430bffd642978fe0f212
sovi <sovialfina@gmail.com>
2025-07-26 21:45:50 +0700

    oop dasar

---
+++ OOP/main.py
@@ -0,0 +1,10 @@
+class mobil: #nama class
+    warna = "hitam" #atribut
+
+mobil_1 = mobil() #objek
+print(mobil_1.warna)
+
+# dapat mengubah value atribut 
+mobil_2 = mobil()
+mobil_2.warna = "ungu"
+print(mobil_2.warna)
